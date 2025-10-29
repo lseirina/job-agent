246ed8a8258de9d8c78d04e0b86438ea0f1c34db
root <lseirina87@gmail.com>
2025-10-29 14:47:29 +0300

    Added extract_experience

--- app/services/resume_parser.py
+++ app/services/resume_parser.py
@@ -8,7 +8,7 @@ import json
 
 class ResumParser:
     def __init__(self, resum_path):
-        self.tresum_path = resum_path
+        self.resum_path = resum_path
         self.skills = []
         self.experience = []
         self.keywords = []
@@ -25,6 +25,7 @@ class ResumParser:
         self.stop_words = set(stopwords.words('russian') + stopwords.words('english'))
         
     def parse_resume(self):
+        "Parsing resume and headlighting skills"
         with open(self.resum_part, 'r', encoding='utf-8') as file:
             content = file.read()
             
@@ -42,6 +43,7 @@ class ResumParser:
             }
         
     def _extract_section(self, content, section_names):
+        "Extract section according to name"
         content_lower = content.lower()
         for name in section_names:
             pattern = rf'{name}[\s\S]*?(?=\n\s*\n|$)'
@@ -49,3 +51,39 @@ class ResumParser:
             if match:
                 return match.group()
         return ""
+    
+    def _extract_skills(self, text):
+        "Extract skills from text"
+        common_skills = ["SQL", "HTML", "Python", "Git", "PostgreSQL", "СУБД", "Linux", 
+        "REST", "HTTP", "API", "Nginx", "Docker", "Docker-compose",
+        "Django Framework", "Amazon Web Services", "Асинхронное программирование",
+        "создания веб-приложений (Flask, Django)", "Django Filters", "Swagger",
+        "Работа с базами данных", "Базы данных", "REST API", "PEP8", "SOLID",
+        "Backend", "Алгоритмы и структуры данных", "Design Patterns", "JSON",
+        "Alembic", "Gitlab"]
+        
+        found_skills = []
+        for skill in common_skills:
+            if skill.lower() in text.lower():
+                found_skill.append(skill)
+        
+        return found_skills
+    
+    def _extract_experience(self, text):
+        experience_patterns = [
+            r'(/d+)/s*(года[а]?|лет|г.)',
+            r'(/d+)/s*месяц[а-я]*',
+        ]
+        found_experience = []
+        for parttern in experience_patterns:
+            matches = re.findall(pattern, text.lower())
+            for match in matches:
+                if isinstance(match, tuple):
+                    experience = ''.join(match)
+                    found_experience.append(experience)
+                else:
+                    found_experience.append(match)
+                    
+        return found_experience                         
+    
+              
